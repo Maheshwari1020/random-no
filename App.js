e0ced537146a83d78a0e3dfdaf53d72a8d276919
Maheshwari1020 <130125115+Maheshwari1020@users.noreply.github.com>
2023-05-17 15:18:02 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,44 @@
+/* Write your CSS here */
+
+.app-container {
+  background-image: url('https://assets.ccbp.in/frontend/react-js/random-no-generator-bg.png');
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  background-color: #ffffff;
+  height: 50vh;
+  width: 50vw;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 10px;
+  text-align: center;
+}
+.heading {
+  color: #0b69ff;
+  font-size: 30px;
+  font-weight: 600;
+  font-family: 'Roboto';
+}
+.para {
+  color: #333333;
+  font-size: 26px;
+  font-weight: 300;
+  font-family: 'Roboto';
+}
+.button {
+  background-color: #0b69ff;
+  font-size: 14px;
+
+  font-family: 'Roboto';
+  border: none;
+  border-radius: 8px;
+  color: #ffffff;
+  height: 30px;
+  width: 80px;
+}
